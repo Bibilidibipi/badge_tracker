2f6eedc37ae4d397b3f6a4c7dc3da6f60572cf47
Judy Wacker <judywacker@Judys-MacBook-Pro-2.local>
2024-10-31 12:24:45 -0500

    add images to badges

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,6 @@
 class Badge < ApplicationRecord
     has_many :badge_users, dependent: :destroy
     has_many :users, through: :badge_users
+    has_one_attached :image
     validates :name, presence: true
 end
