e6bb5afc1adfabae5fac59529f4873c51cfcab6d
Judy Wacker <judy0wacker@gmail.com>
2024-11-09 17:39:01 -0600

    add pi deployment script

---
+++ pi_deploy/deploy.sh
@@ -0,0 +1,86 @@
+## Run this on the Raspberry Pi
+## Before running this script, do the following:
+## * Use Raspberry Pi Imager to flash SD card with Raspberry Pi OS Lite (64 bit) (10/22/24)
+##      * with SSH and Wifi enabled, username: badge_tracker
+## * Insert SD card in Pi, and power it up
+## * SSH pi_deploy/deploy.sh (this script) to /tmp/deploy.sh
+## * SSH pi_deploy/badge_tracker.service to /tmp/badge_tracker.service on Pi
+## * SSH pi_deploy/apache.conf to /tmp/apache.conf on Pi
+## * SSH to Pi
+## * execute /tmp/deploy.sh with argument rails_master_key
+
+die () {
+    echo >&1 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Specify rails_master_key as first parameter"
+
+RAILS_MASTER_KEY=$1
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y git libffi-dev libssl-dev libyaml-dev libz-dev
+# sudo apt-get install -y build-essential zlib1g-dev libgdbm6
+
+# Ruby
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+RUBY_CONFIGURE_OPTS="--disable-install-doc" rbenv install 3.3.5 --verbose
+rbenv global 3.3.5
+gem install bundler -v 2.5.22 --no-document
+
+# Apache
+sudo apt install -y apache2
+
+# Rails App
+echo 'export RAILS_ENV=production' >> ~/.bashrc
+export RAILS_ENV=production
+cd /var/www
+sudo git clone https://github.com/Bibilidibipi/badge_tracker.git
+sudo chown $(whoami) -R badge_tracker
+cd badge_tracker
+
+# Master Key in perpetuity
+echo "$RAILS_MASTER_KEY" > /var/www/badge_tracker/config/master.key
+# ??? Exit here and scp config/master.key from local machine to the Pi
+
+# PostgreSQL
+sudo apt install -y postgresql libpq-dev # postgresql-contrib
+sudo -u postgres createuser -s badge_tracker
+sudo -u postgres psql -c "ALTER USER badge_tracker WITH PASSWORD '$(bin/rails runner "p Rails.application.credentials.dig(:production, :database, :password)")';"
+
+# md5 in /etc/postgresql/15/main/pg_hba.conf ? careful with chown
+
+# bundle config build.bcrypt --use-system-libraries
+bundle config set without 'development test'
+bundle install --deployment
+
+bin/rails db:create
+bin/rails db:migrate
+# bin/rails assets:precompile # (don't need with Rails 7 Import Maps)
+
+# systemd to run the app
+sudo mv /tmp/badge_tracker.service /etc/systemd/system/badge_tracker.service
+sudo chown root /etc/systemd/system/badge_tracker.service
+sudo chmod 755 /etc/systemd/system/badge_tracker.service
+
+## Enable the systemd process
+sudo systemctl daemon-reload
+sudo systemctl enable badge_tracker.service
+sudo systemctl start badge_tracker.service
+
+## Apache config file
+sudo mv /tmp/apache.conf /etc/apache2/sites-available/001-badge_tracker.conf
+sudo chown root /etc/apache2/sites-available/001-badge_tracker.conf
+sudo chmod 644 /etc/apache2/sites-available/001-badge_tracker.conf
+
+## Enable new Apache config
+sudo a2enmod rewrite proxy proxy_http headers # ssl
+sudo a2ensite 001-badge_tracker
+sudo a2dissite 000-default
+sudo systemctl restart apache2
